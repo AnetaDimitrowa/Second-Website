407d4ed773bce4ada1bb7d482827153a0cbae1a0
AnetaDimitrowa <81328442+AnetaDimitrowa@users.noreply.github.com>
2021-03-24 21:52:34 +0200

    Add files via upload

---
+++ vtorisait.js
@@ -0,0 +1,15 @@
+function closeMenu() {
+    document.getElementById("navbar").style.height = "0%";
+}
+
+function openMenu() {
+    document.getElementById("navbar").style.height = "100%";
+}
+
+function openbutton() {
+    document.getElementById("buttonopen").style.height = "100%";
+}
+
+function closebutton() {
+    document.getElementById("buttonopen").style.height = "0%";
+}
